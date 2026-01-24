b5f04506492bf6efa750c637c039e3e9ae157edc
Jaine Oliveira <jaine.ojuacy@gmail.com>
2026-01-24 12:36:11 -0300

    Alterado ícone de fechamento mensal

--- resources/views/partials/dashboard/vertical-nav.blade.php
+++ resources/views/partials/dashboard/vertical-nav.blade.php
@@ -181,7 +181,7 @@
         <a class="nav-link {{activeRoute(route('monthly-closures.index'))}}" href="{{route('monthly-closures.index')}}">
             <i class="icon">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/>
+                    <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z"/>
                 </svg>
             </i>
             <span class="item-name">Fechamento Mensal</span>
